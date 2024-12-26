1e8e9e06fcad0bfa89ac7bdc7908e51b742bd2de
corafan <corafan55u@gmail.com>
2024-12-26 14:57:34 +0800

    add more details

--- gdsfactory/generic_tech/layer_map.py
+++ gdsfactory/generic_tech/layer_map.py
@@ -3,6 +3,7 @@ import gdsfactory as gf
 Layer = tuple[int, int]
 
 
+#这部分是给Layer起个名字，让layer可以被调用，用于组件的描述
 class LAYER(gf.LayerEnum):
     """Generic layermap based on book.
 
@@ -63,13 +64,51 @@ class LAYER(gf.LayerEnum):
     TM: Layer = (204, 0)
     DRC_MARKER: Layer = (205, 0)
     
-#新添加的层
+    SOURCE: Layer = (110, 0)
+    MONITOR: Layer = (101, 0)
+
+
+
+#新添加的层--用于测试
     csulayer1:Layer = (400,0)
     csulayer2:Layer = (401,0)
     csulayer3:Layer = (402,0)
 
-    SOURCE: Layer = (110, 0)
-    MONITOR: Layer = (101, 0)
+#新建关于工艺流程的层，如下：
+    Si_Sub: Layer = (88, 0)
+    SiO_Bottom_Clad: Layer = (87,0)
+
+    SiO_WG_1_Core: Layer = (1,1)
+    SiO_WG_1_Clad: Layer = (1,2)
+    SiO_WG_2_Core: Layer = (2,1)
+    SiO_WG_2_Clad: Layer = (2,2)
+    SiO_WG_3_Core: Layer = (3,1)
+    SiO_WG_3_Clad: Layer = (3,2)
+
+    SiO_ToP_Clad: Layer = (4,0)
+    Metal_TiN: Layer = (10,0)
+    SiO_Oxide_1: Layer = (11,0)
+    Metal_Al: Layer = (12,0)
+    Metal_Ti: Layer = (13,0)
+    SiN: Layer = (14,0)
+
+    #Dopping：
+    NWD: Layer = (30,0)
+    PWD: Layer = (31,0)
+    ND1: Layer = (32,0)
+    PD1: Layer = (33,0)
+    ND2: Layer = (34,0)
+    PD2: Layer = (35,0)
+    ND_Ohmic: Layer = (36,0)
+    PD_Ohmic: Layer = (37,0)
+
+    #注释部分：
+    Label_Optical_IO: Layer = (95,0)
+    Label_Settings: Layer = (96,0)
+    TXT: Layer = (97,0)
+    DA: Layer = (98,0)
+    DecRec: Layer = (99,0)
+
 
 
 gf.kcl.layers = LAYER
