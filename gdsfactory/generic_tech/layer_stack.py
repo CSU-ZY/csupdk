032ce54bbe5a16fe1d36f04a81a01fe6edbf3021
corafan <corafan55u@gmail.com>
2025-03-07 22:44:54 +0800

    3.7版

--- gdsfactory/generic_tech/layer_stack.py
+++ gdsfactory/generic_tech/layer_stack.py
@@ -16,21 +16,25 @@ class LayerStackParameters:
     """values used by get_layer_stack and get_process."""
     """用于层栈和工艺"""
 
+#后续核对：
     thickness_substrate: float = 675                   #基板厚度um
     thickness_bottom_clad: float = 15                  #下包层——镀层厚度um
     thickness_wg: float = 220 * nm                     #波导层厚度(core)
-    thickness_slab_full_etch: float = 0 * nm           #相当于trench层，全刻蚀，刻蚀深度220nm(SiO_WG_1_Core)
-    thickness_slab_deep_etch: float = 90 * nm          #深刻蚀，刻蚀深度130nm(SiO_WG_2_Core)
-    thickness_slab_shallow_etch: float = 150 * nm      #浅刻蚀，刻蚀深度70nm(SiO_WG_3_Core)
+    sidewall_angle_wg: float = 0                       #侧壁倾斜角度
+    thickness_wgn: float = 220 * nm                    #非线性波导层厚度(core)
+    sidewall_angle_wgn: float = 0                      #侧壁倾斜角度
+    thickness_slab_full_etch: float = 0 * nm           #相当于trench层，全刻蚀，刻蚀深度220nm
+    thickness_slab_deep_etch: float = 90 * nm          #深刻蚀，刻蚀深度130nm
+    thickness_slab_shallow_etch: float = 150 * nm      #浅刻蚀，刻蚀深度70nm
     thickness_top_clad: float = 20                     #上包层——镀层厚度um
 
     thickness_metal_TiN: float = 2000 * nm             #TiN加热层厚度：？
-    zmin_heater: float = 1.1                           #?
+    #zmin_heater: float = 1.1                           #位置?
     thickness_heater_clad: float = 20                  #加热层TiN_氧化层um
     thickness_metal_Ti: float = 1400 * nm              #间隔层厚度
-    zmin_metal_Ti: float = 1.1                         #?
+    #zmin_metal_Ti: float = 1.1                         #位置?
     thickness_metal_Al: float = 700 * nm               #电极层Al的厚度：？
-    zmin_metal_Al: float = 2.3                         #?
+    #zmin_metal_Al: float = 2.3                         #位置?
     thickness_SiN: float = 350 * nm                    #保护层SiN厚度：？
 
 
@@ -39,28 +43,32 @@ def get_layer_stack(
     thickness_substrate: float = LayerStackParameters.thickness_substrate,
     thickness_bottom_clad: float = LayerStackParameters.thickness_bottom_clad,    
     thickness_wg: float = LayerStackParameters.thickness_wg,
+    sidewall_angle_wg: float = LayerStackParameters.sidewall_angle_wg,
+    thickness_wgn: float = LayerStackParameters.thickness_wgn,
+    sidewall_angle_wgn: float = LayerStackParameters.sidewall_angle_wgn,
     thickness_slab_full_etch: float = LayerStackParameters.thickness_slab_full_etch,
     thickness_slab_deep_etch: float = LayerStackParameters.thickness_slab_deep_etch,
     thickness_slab_shallow_etch: float = LayerStackParameters.thickness_slab_shallow_etch,
     thickness_top_clad: float = LayerStackParameters.thickness_top_clad,  
     thickness_metal_TiN: float = LayerStackParameters.thickness_metal_TiN,
-    zmin_heater: float = LayerStackParameters.zmin_heater,
+    #zmin_heater: float = LayerStackParameters.zmin_heater,
     thickness_heater_clad: float = LayerStackParameters.thickness_heater_clad,
-    zmin_metal_Ti: float = LayerStackParameters.zmin_metal_Ti,
+    #zmin_metal_Ti: float = LayerStackParameters.zmin_metal_Ti,
     thickness_metal_Ti: float = LayerStackParameters.thickness_metal_Ti,
-    zmin_metal_Al: float = LayerStackParameters.zmin_metal_Al,
+    #zmin_metal_Al: float = LayerStackParameters.zmin_metal_Al,
     thickness_metal_Al: float = LayerStackParameters.thickness_metal_Al,
     thickness_SiN: float = LayerStackParameters.thickness_SiN,
 
-    layer_Si_Sub: LogicalLayer = LogicalLayer(layer=LAYER.Si_Sub), #Si基板
-    layer_bottom_clad: LogicalLayer = LogicalLayer(layer=LAYER.SiO_Bottom_Clad), #氧化层下包层
-    layer_core: LogicalLayer = LogicalLayer(layer=LAYER.WG), #波导核心部分
-    layer_slab_full_etch: LogicalLayer = LogicalLayer(layer=LAYER.SiO_WG_1_Slab),
-    layer_full_etch: LogicalLayer = LogicalLayer(layer=LAYER.SiO_WG_1_Clad), 
-    layer_slab_shallow_etch: LogicalLayer = LogicalLayer(layer=LAYER.SiO_WG_3_Slab),
-    layer_shallow_etch: LogicalLayer = LogicalLayer(layer=LAYER.SiO_WG_3_Clad),
-    layer_slab_deep_etch: LogicalLayer = LogicalLayer(layer=LAYER.SiO_WG_2_Slab),
-    layer_deep_etch: LogicalLayer = LogicalLayer(layer=LAYER.SiO_WG_2_Clad),
+    layer_Si_Sub: LogicalLayer = LogicalLayer(layer=LAYER.Si_Sub), 
+    layer_box: LogicalLayer = LogicalLayer(layer=LAYER.SiO_Bottom_Clad), 
+    layer_core: LogicalLayer = LogicalLayer(layer=LAYER.WG), 
+    layer_core_wgn: LogicalLayer = LogicalLayer(layer=LAYER.WGN), 
+    layer_slab_full_etch: LogicalLayer = LogicalLayer(layer=LAYER.SLAB0),
+    layer_full_etch: LogicalLayer = LogicalLayer(layer=LAYER.Full_Etch), 
+    layer_slab_shallow_etch: LogicalLayer = LogicalLayer(layer=LAYER.SLAB150),
+    layer_shallow_etch: LogicalLayer = LogicalLayer(layer=LAYER.Shallow_Etch),
+    layer_slab_deep_etch: LogicalLayer = LogicalLayer(layer=LAYER.SLAB90),
+    layer_deep_etch: LogicalLayer = LogicalLayer(layer=LAYER.Deep_Etch),
     layer_top_clad: LogicalLayer = LogicalLayer(layer=LAYER.SiO_ToP_Clad),
     layer_metal_TiN: LogicalLayer = LogicalLayer(layer=LAYER.Metal_TiN),
     layer_heater_clad: LogicalLayer = LogicalLayer(layer=LAYER.SiO_Oxide_1),
@@ -76,9 +84,10 @@ def get_layer_stack(
         thickness_substrate: substrate thickness in um.
         thickness_bottom_clad: bottom cladding thickness in um.
         thickness_wg: waveguide thickness in um.
+        thickness_wng: waveguide thickness in um.
         
-        thickness_slab_full_etch: slab thickness after full etch in um. equal to thickness of trench.
-        thickness_slab_deep_etch: slab thickness after deep etch in um.
+    
+        thickness_slab_deep_etch: slab thickness after deep etch in um. equal to thickness of trench.
         thickness_slab_shallow_etch: slab thickness after shallow etch in um.
         thickness_top_clad: top cladding thickness in um.
         thickness_metal_TiN: metal TiN thickness.
@@ -93,7 +102,7 @@ def get_layer_stack(
         layer_Si_Sub: substrate layer.
         layer_bottom_clad: SiO2 bottom cladding layer.
         layer_core: waveguide layer.
-        layer_slab_full_etch: full etch slab layer.    
+        layer_core_wng: waveguide layer. 
         layer_full_etch: full etch layer.
         layer_slab_deep_etch: deep etch slab layer.        
         layer_deep_etch: deep etch layer.      
@@ -106,126 +115,132 @@ def get_layer_stack(
         layer_metal_Al: metal Al layer.
         layer_SiN: SiN layer for protection.
     """
-    thickness_full_etch = thickness_wg - thickness_slab_full_etch         #全刻蚀深度
+    thickness_full_etch = thickness_wg - thickness_slab_deep_etch         #全刻蚀深度
     thickness_deep_etch = thickness_wg - thickness_slab_deep_etch         #深刻蚀深度
     thickness_shallow_etch = thickness_wg - thickness_slab_shallow_etch   #浅刻蚀深度
 
     layers = dict(
         substrate=LayerLevel(
             layer=layer_Si_Sub,
-            thickness=thickness_substrate,
-            zmin=-thickness_substrate - 0, #box_thickness
+            thickness=-thickness_substrate-thickness_bottom_clad,
+            zmin=0,                           #box_thickness
             material="si",
-            mesh_order=101,#?
+            mesh_order=101,                   #网格划分，数字小的优先，用于数值仿真
         ),
         box=LayerLevel(
-            layer=layer_Si_Sub,
-            thickness= 0, #box_thickness
-            zmin= 0, #-box_thickness
+            layer=layer_box,
+            thickness=thickness_bottom_clad,  
+            zmin=-thickness_bottom_clad,                          
             material="sio2",
             mesh_order=9,
         ),        
-        bottom_clad=LayerLevel(
-            layer=layer_bottom_clad,    
-            zmin=0.0,
-            material="sio2",
-            thickness=thickness_bottom_clad,
-            mesh_order=10,
-        ),
         core=LayerLevel(
-            layer=layer_core - layer_deep_etch - layer_shallow_etch,  #???
+            layer=layer_core - layer_full_etch - layer_deep_etch - layer_shallow_etch,
+            #这层是由多个 GDS 层组合而成的物理派生层，通过计算得出最终层。第一层是核心层。
             thickness=thickness_wg,
-            zmin=0.0,
+            zmin=0,
             material="si",
             mesh_order=2,
-           #sidewall_angle=sidewall_angle_wg,
+            sidewall_angle=sidewall_angle_wg,
             width_to_z=0.5,
             derived_layer=LogicalLayer(layer=LAYER.WG),
+            #最终计算出来的层，映射到GDS的WG层
+        ),
+        core_wgn=LayerLevel(
+            layer=layer_core_wgn - layer_full_etch - layer_deep_etch - layer_shallow_etch,
+            thickness=thickness_wgn,
+            zmin=0,
+            material="si",
+            mesh_order=2,
+            sidewall_angle=sidewall_angle_wgn,
+            width_to_z=0.5,
+            derived_layer=LogicalLayer(layer=LAYER.WGN),
         ),
         shallow_etch=LayerLevel(
-            layer=layer_shallow_etch & layer_core,    #???
-            thickness=thickness_shallow_etch,
-            zmin=0.0,
+            layer=layer_shallow_etch & layer_core,    
+            thickness=0,
+            zmin=thickness_wgn,
             material="si",
             mesh_order=1,
-            derived_layer=LogicalLayer(layer=LAYER.SiO_WG_3_Slab),#???
+            derived_layer=LogicalLayer(layer=LAYER.SLAB150),
+            #这部分，意思是core和shallow etch的重叠部分，最后由SLAB150从z=0长出来Si,相当于也是芯层。
         ),
         deep_etch=LayerLevel(
-            layer=layer_deep_etch,
+            layer=layer_deep_etch & layer_core, 
             thickness=thickness_deep_etch,
             zmin=0.0,
             material="si",
             mesh_order=1,
-            derived_layer=layer_slab_deep_etch, ##???
+            derived_layer=LogicalLayer(layer=LAYER.SLAB90), 
         ),
         full_etch=LayerLevel(
-            layer=layer_full_etch,
+            layer=layer_full_etch & layer_core,
             thickness=thickness_full_etch,
             zmin=0.0,
             material="si",
             mesh_order=1,
-            derived_layer=layer_slab_full_etch, ##??
+            derived_layer=LogicalLayer(layer=LAYER.SLAB0), 
         ),
-        slab_shallow_etch=LayerLevel(            #slab150
+        slab_shallow_etch=LayerLevel(              #slab150
             layer=layer_slab_shallow_etch,
-            thickness=150e-3,
+            thickness=thickness_shallow_etch,
             zmin=0,
             material="si",
-            mesh_order=3,
+            mesh_order=2,
         ),
         slab_deep_etch=LayerLevel(                  #slab90
             layer=layer_slab_deep_etch,
             thickness=thickness_slab_deep_etch,
             zmin=0.0,
             material="si",
-            mesh_order=2,
+            mesh_order=3,
         ),
-        slab_full_etch=LayerLevel(                  #???slab0，全刻蚀后不存在slab了
+        slab_full_etch=LayerLevel(                  
             layer=layer_slab_full_etch,
             thickness=thickness_slab_full_etch,
             zmin=0.0,
             material="si",
-            mesh_order=2,
+            mesh_order=4,
         ),
         top_clad=LayerLevel(
             layer=layer_top_clad,                    
-            zmin=0.0,
+            zmin=0,                                       #？这部分还有待考量，分不同情况，看怎么安排
             material="sio2",
-            thickness=thickness_top_clad,
+            thickness=thickness_top_clad,                  #+ thickness_wg？#同上
             mesh_order=10,
         ),
         TiN=LayerLevel(                        
             layer=layer_metal_TiN,
-            thickness=thickness_metal_TiN,
-            zmin=thickness_wg + 0, #gap_silicon_to_nitride??
+            thickness=thickness_metal_TiN,                 
+            zmin=thickness_wg + thickness_top_clad,
             material="TiN",
             mesh_order=2,
         ),
         heater_clad=LayerLevel(
             layer=layer_heater_clad,               
-            zmin=0.0,
+            zmin=thickness_wg + thickness_top_clad,       #因为这里只有一种情况，全刻蚀            
             material="sio2",
-            thickness=thickness_heater_clad,
-            mesh_order=10,
+            thickness=thickness_heater_clad,              
+            mesh_order=2,
         ),
         Ti=LayerLevel(
             layer=layer_metal_Ti,
             thickness=thickness_metal_Ti,
-            zmin=zmin_metal_Ti,
+            zmin=thickness_wg + thickness_top_clad + thickness_metal_TiN,
             material="Titanium",
-            mesh_order=1,
+            mesh_order=2,
         ),
         Al=LayerLevel(
             layer=layer_metal_Al,
             thickness=thickness_metal_Al,
-            zmin=zmin_metal_Al,          
+            zmin=thickness_wg + thickness_top_clad + thickness_metal_TiN + thickness_metal_Ti,          
             material="Aluminum",
-            mesh_order=1,
+            mesh_order=2,
         ),
         SiN=LayerLevel(
             layer=layer_SiN,
             thickness=thickness_SiN,
-            zmin=zmin_heater,
+            zmin=thickness_wg + thickness_top_clad + thickness_metal_TiN + thickness_heater_clad + thickness_metal_Ti,
             material="SiN",
             mesh_order=2,
         ),
@@ -242,7 +257,8 @@ WAFER_STACK = LayerStack(
         for k in (
             "substrate",
             "box",
-            #"core",
+            "core",
+            "core_wgn",
         )
     }
 )
@@ -257,18 +273,18 @@ def get_process() -> tuple[ProcessStep, ...]:
     """
     return (
         Etch(
-            name="strip_etch",                   #220nm波导层
+            name="strip_etch",                #220nm波导层
             layer=LAYER.WG,
             layers_or=[LAYER.SiO_WG_2_Slab],
-            #mask open = WG + SiO_WG_2_Slab
+                                              #mask open = WG + SiO_WG_2_Slab
             depth=LayerStackParameters.thickness_wg
-            + 0.01,                              # slight overetch for numerics 轻微刻深一点
+            + 0.01,                           # slight overetch for numerics 轻微刻深一点
             material="silicon",
-            resist_thickness=1.0,                #光刻胶厚度
-            positive_tone=False,                 #flase代表负胶
+            resist_thickness=1.0,             #光刻胶厚度
+            positive_tone=False,              #flase代表负胶
         ),
         Etch(
-            name="slab_etch",                    #波导上的刻蚀，slab
+            name="slab_etch",                 #波导上的刻蚀，slab
             layer=LAYER.SiO_WG_2_Slab,
             layers_diff=[LAYER.WG],
             depth=LayerStackParameters.thickness_wg
@@ -287,13 +303,13 @@ def get_process() -> tuple[ProcessStep, ...]:
         ImplantPhysical(
             name="deep_NWD_implant",
             layer=LAYER.NWD,
-            energy=100,                         #指定离子的能量，单位通常是电子伏特（eV）
-            ion="P",                            #注入的离子种类，用于N型掺杂
-                                                #"As",表示注入的是砷离子。
-                                                #"P",表示注入的是磷离子（Phosphorus）。磷离子通常用于掺杂硅以调节其导电性。
+            energy=100, #指定离子的能量，单位通常是电子伏特（eV）
+            ion="P", #注入的离子种类，用于N型掺杂
+            #"As",表示注入的是砷离子。
+            #"P",表示注入的是磷离子（Phosphorus）。磷离子通常用于掺杂硅以调节其导电性。
             dose=1e12,
-                                                #dose 参数表示注入的离子数量，单位通常是每单位面积的离子数量。这里设置为 1e12，即每单位面积注入 10的12次方个离子。
-            resist_thickness=1.0,               #光刻胶的厚度
+            #dose 参数表示注入的离子数量，单位通常是每单位面积的离子数量。这里设置为 1e12，即每单位面积注入 10的12次方个离子。
+            resist_thickness=1.0,#光刻胶的厚度
         ),
         ImplantPhysical(
             name="shallow_NWD_implant",
@@ -307,9 +323,9 @@ def get_process() -> tuple[ProcessStep, ...]:
             name="deep_PWD_implant",
             layer=LAYER.PWD,
             energy=50,
-            ion="B",                            #用于P型掺杂
-                                                #"B",表示硼离子
-                                                #"Al",铝离子也可以用于P型掺杂
+            ion="B", #用于P型掺杂
+            #"B",表示硼离子
+            #"Al",铝离子也可以用于P型掺杂
             dose=1e12,
             resist_thickness=1.0,
         ),
@@ -406,7 +422,7 @@ def get_process() -> tuple[ProcessStep, ...]:
             - LayerStackParameters.thickness_slab_deep_etch,
             material="Titanium",
             type="anisotropic",
-        ),      
+        ),  
         Grow(
             name="viac_metallization1", #金属化，Al
             layer=None,
@@ -414,7 +430,7 @@ def get_process() -> tuple[ProcessStep, ...]:
             - LayerStackParameters.thickness_slab_deep_etch,
             material="Aluminum",
             type="anisotropic",
-        ),         
+        ),    
         Planarize(
             name="planarization",
             height=LayerStackParameters.thickness_top_clad
@@ -431,7 +447,4 @@ if __name__ == "__main__":
     # print(ls.get_layer_to_thickness())
 
     for layername, layer in WAFER_STACK.layers.items():
-        print(layername, layer.zmin, layer.thickness)
-
-    for layername, layer in LAYER_STACK.layers.items():
         print(layername, layer.zmin, layer.thickness)
